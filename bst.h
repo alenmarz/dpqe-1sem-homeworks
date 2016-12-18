6687b4a257407a2a02340938bbee7d5d6dc12980
alenmarz <english52009@yandex.ru>
2016-12-19 01:53:12 +0400

    Update bst.h

--- bst.h
+++ bst.h
@@ -1,21 +1,21 @@
 typedef struct Node Node;
 
 struct Node {
-    void * key;
-    void * value;
+    void* key;
+    void* value;
 
-    Node * left;
-    Node * right;
-    Node * parent;
+    Node* left;
+    Node* right;
+    Node* parent;
 };
 
 
 typedef struct Tree {
-    Node * root;
-    int (*compare_keys)(void *, void *);
-    void (*destroy_node_hook)(Node *);
-    void (*insert_node_hook)(Node *);
-    Node * (*remove_node_hook)(Node *);
+    Node* root;
+    int (*compare_keys)(void*, void*);
+    void (*destroy_node_hook)(Node*);
+    void (*insert_node_hook)(Node*);
+    Node* (*remove_node_hook)(Node*);
 } Tree;
 
 
@@ -27,27 +27,27 @@ typedef enum Order {
 
 
 void tree_init(
-    Tree * tree,
-    int (*compare_keys)(void *, void *), 
-    void (*destroy_node_hook)(Node *),
-    void (*insert_node_hook)(Node *),
-    Node * (*remove_node_hook)(Node *)
+    Tree* tree,
+    int (*compare_keys)(void*, void*), 
+    void (*destroy_node_hook)(Node*),
+    void (*insert_node_hook)(Node*),
+    Node* (*remove_node_hook)(Node*)
 );
 
-void tree_destroy(Tree * tree);
-void tree_insert(Tree * tree, void * key, void * value);
-void * tree_lookup(Tree * tree, void * key);
-void tree_remove(Tree * tree, void * key);
+void tree_destroy(Tree* tree);
+void tree_insert(Tree* tree, void* key, void* value);
+void* tree_lookup(Tree* tree, void* key);
+void tree_remove(Tree* tree, void* key);
 
-void default_destroy_node_hook(Node * node);
-void default_insert_node_hook(Node * node);
-Node * default_remove_node_hook(Node * node);
+void default_destroy_node_hook(Node* node);
+void default_insert_node_hook(Node* node);
+Node * default_remove_node_hook(Node* node);
 
 void tree_traverse(
-    Tree * tree,
-    void (*visit_node)(Node *),
+    Tree* tree,
+    void (*visit_node)(Node*),
     Order order
 );
 
-void bfs(Tree * tree, void (*visit_node)(Node *));
-void dfs(Tree * tree, void (*visit_node)(Node *));
+void bfs(Tree* tree, void (*visit_node)(Node*));
+void dfs(Tree* tree, void (*visit_node)(Node*));
